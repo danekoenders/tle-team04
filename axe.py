acf5e47cc9e681c8be3641607fd6114f2c5b5cb4
Yasar Unlu <57659697+hro-ict@users.noreply.github.com>
2023-01-15 15:19:20 +0100

    Update axe.py

--- axe.py
+++ axe.py
@@ -32,17 +32,11 @@ def scan_website(url):
 
     # Run axe accessibility checks.
     results = axe.run()
-    # Write results to file
+
     axe.write_results(results["violations"], 'a11.json')
-    # sites.insert_one({"site_name":url, "issues_count": len(results["violations"]), "issues": results["violations"]})
+   
     driver.close()
 
-
-    # driver.quit()
-    # Assert no violations are found
-    #assert len(results["violations"]) == 0, axe.report(results["violations"])
-    #report= axe.report(results["violations"])
-    # print(report)
 
 
 
